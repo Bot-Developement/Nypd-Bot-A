7596dc6552597dbab49c5dd1aad56daaabba10d0
XXWolfBane <XXWolfBane@Gmail.com>
2018-07-05 14:59:49 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,8 +13,8 @@ async def on_ready():
 @bot.command()
 async def kick(ctx, userName: discord.User):
     if True: ctx.message.author.Permissions.administrator
-        await BSL.kick(userName)
+    await BSL.kick(userName)
     else:
-        permission_error = str('Sorry ' + ctx.message.author + ' you do not have permissions to do that!')
-        await BSL.send_message(ctx.message.channel, permission_error)
+    permission_error = str('Sorry ' + ctx.message.author + ' you do not have permissions to do that!')
+    await BSL.send_message(ctx.message.channel, permission_error)
 bot.run('NDYxOTUzNTk5NDI2MDAyOTU2.Dh_47w.V34Te-LudmfY9aPGGvkml4MYAKo')
